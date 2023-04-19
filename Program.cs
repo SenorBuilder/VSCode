ccc173838f5ff11fab500a84647a0bc6d98ac75f
SenorBuilder <finnwjanik@gmail.com>
2023-04-18 20:22:55 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VSCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string choice;
+
+            while(true){
+                choice = "";
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Quadratic Formula (QF)");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Please enter the abreviation for which program you would like to use:");
+                Console.ForegroundColor = ConsoleColor.Blue;
+
+                while(choice != "qf"){
+                    choice = Console.ReadLine().ToLower();
+                    if(choice == "qf"){
+                        Console.Clear();
+                        QuadraticFormula.QF();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ QuadraticFormula.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VSCode
+{
+    internal class QuadraticFormula
+    {
+
+        public static void QF()
+        {
+            double a;
+            double b;
+            double c;
+
+            double A;
+            double _B;
+            double B;
+            double C;
+
+            double PAnswer;   
+            double NAnswer;
+
+            Console.WriteLine("\n    -b +- √b² - 4(a)(c)");
+            Console.WriteLine("x = -------------------");
+            Console.WriteLine("           2(a)        ");
+
+            Console.WriteLine("\nInsert a number for 'a'");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nInsert a number for 'b'");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nInsert a number for 'c'");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            A = b * -1;
+            _B = b * b - 4 * a * c;
+            if(_B < 0){
+                _B = _B * -1;
+            }
+            _B = Math.Sqrt(_B) * -1;
+            B = Math.Round(_B, 3);
+            C = a * 2;
+
+            Console.WriteLine("\nThe unsimplified answer is: \n ");
+            Console.WriteLine("    " + A + " +- " + B);
+            Console.WriteLine("x = ------------");
+            Console.WriteLine("         "+ C);
+
+            PAnswer = A + B;
+            PAnswer = Math.Round(PAnswer / C, 3);
+
+            Console.WriteLine("The positive simplified answer is: \n" + PAnswer);
+
+            NAnswer = A - B;
+            NAnswer = Math.Round(NAnswer / C, 3);      
+
+            Console.WriteLine("\nThe negative simplified answer is: \n" + NAnswer);
+            
+
+            Console.ReadKey();
+            Console.Clear();
+        }
+        
+    }
+}
